165df0ac73c1dbd60ef3ef81e6672533f12989c5
li-xiaoyu960 <76767955+li-xiaoyu960@users.noreply.github.com>
2023-01-09 18:15:43 +0800

    new

--- ip_update_autorun.sh
+++ ip_update_autorun.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#echo "UTF-8 is græat ☺"
+#echo "UTF-8 is græat  xM-:"
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/etc/openclash
-export LANG="en_US.UTF-8" 
+export LANG="en_US.UTF-8"
 
-rm /etc/openclash/ip_update_latest.json
+rm /etc/openclash/proxy_provider/ip_update_latest.json
 sleep 30
-sh /etc/openclash/ip_update_download_decode.sh "https://XXXXXXXXXXXXXXX"
+sh /etc/openclash/proxy_provider/ip_update_download_decode.sh "XXXXXXXXXXXXX"
 sleep 30
-sh /etc/openclash/ip_update_sed.sh >>/etc/openclash/ip_update_log.log
+sh /etc/openclash/proxy_provider/ip_update_sed.sh >>/etc/openclash/proxy_provider/ip_update_log.log
 sleep 30
 
 exit 0
